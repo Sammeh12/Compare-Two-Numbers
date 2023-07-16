be0048fcba82173ba883a708e660556455b7818e
Sammeh12 <samuel.gamay12@gmail.com>
2023-07-16 15:42:30 +0800

    Compare Two Numbers

---
+++ main.py
@@ -0,0 +1,9 @@
+num1 = input("Enter Num 1: ")
+num2 = input("Enter Num 2: ")
+
+if num1 < num2:
+    print("Num2 is greater than Num1")
+else:
+    print("Num2 is less than Num1")
+
+print("The variables are num1 and num2. The bindings are both dynamic")
